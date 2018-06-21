08fa7a575d3a480d0fa41f995d09aae7d38f9fdb
Dave Moore <dmoore@jplcreative.com>
2018-06-21 04:05:27 -0400

    Cleaning up code and adding comments

--- js/dist/main/main.js
+++ js/dist/main/main.js
@@ -1,16 +1,11 @@
 // JavaScript Document
 
-// AlgaeCal's API
-
+// ALGAECAL API CALL
 var request = new XMLHttpRequest();
-
 request.open('GET', 'https://www.algaecal.com/wp-json/acf/v3/options/options/default_phone_number', true);
-
 request.onload = function () {
-
-"use strict";
-var data = JSON.parse(this.response);
-
+  "use strict";
+  var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
       $( ".api-phone" ).html( data.default_phone_number );
       $( ".api-phone" ).attr("href", "tel:"+data.default_phone_number);
@@ -18,6 +13,7 @@ var data = JSON.parse(this.response);
 };
 request.send();
 
+// CHECK BUSINESS HOURS (BASIC: 7-4)
 var date = new Date();
 var day = date.getDay();
 var daystring = parseInt(day);
@@ -31,6 +27,7 @@ console.log(daystring);
      $(".bone-health-specialist").hide();
   }
 
+// VIDEO LINK
 $('.play-video').click(function() {
   "use strict";
   $(this).html('<div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_cecdwaq3dz videoFoam=true " style="height:100%;width:100%">&nbsp;</div></div></div>');
